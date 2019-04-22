0667b1dbb30340ab26694fe9fd1f006ba0d8471e
samirdadashzade <samir.d@code.edu.az>
2019-04-22 13:03:32 +0400

    Files uploaded

---
+++ src/main.js
@@ -0,0 +1,48 @@
+window.onload = function()
+{
+    if(localStorage.getItem("basket") === null)
+    {
+        localStorage.setItem("basket", JSON.stringify([]));
+    }
+    UpdateBasketIcon();
+
+    const productButtons = document.querySelectorAll('.btn-product');
+
+    [...productButtons].forEach(pro => {
+        pro.onclick = function(e)
+        {
+            e.preventDefault();
+            const proId = this.parentElement.getAttribute("data-id");
+
+            const basket = JSON.parse(localStorage.getItem("basket"));
+
+            const basketElement = basket.find(el => {
+                return el.id === proId;
+            })
+
+            if(basketElement === undefined)
+            {
+                basket.push({
+                    id: proId,
+                    count: 1
+                })
+            }
+            else{
+                basketElement.count++;
+            }
+            
+            //update localstorage to include new product
+            localStorage.setItem("basket", JSON.stringify(basket));
+            UpdateBasketIcon();
+        }
+    })
+
+}
+
+
+function UpdateBasketIcon()
+{
+    const basket = JSON.parse(localStorage.getItem("basket"));
+    document.querySelector('#basket-count').innerText = basket.length;
+    // document.querySelector('#basket-count').innerText = basket.reduce((t, p) => t + p.count, 0);
+}
